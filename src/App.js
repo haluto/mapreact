b95f3f8ddab487dcc886cbbcf2d55bae6312aeb5
terry.yin <haluto8086@sina.cn>
2019-03-08 14:17:10 +0800

    add some css

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Input } from 'antd';
 import MapLoader from "./js/components/MapLoader";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <MapLoader />
+
+        <div className="head-bar">
+          <Input.Search
+            placeholder="input search text"
+            enterButton="Search"
+            size="large"
+            onSearch={value=>console.log(value)}
+            style={{ width: 200 }}
+          />
+        </div>
+
+        <div className="map-area">
+          <MapLoader />
+        </div>
       </div>
     );
   }
