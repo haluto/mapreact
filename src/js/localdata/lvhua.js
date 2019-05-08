838716334895ef2e758d89406aa88d675961c25d
terry.yin <haluto8086@sina.cn>
2019-05-08 17:14:51 +0800

    add lvhua json.

---
+++ src/js/localdata/lvhua.js
@@ -0,0 +1,215 @@
+export default getLvHuaData;
+
+function getLvHuaData() {
+  return LvHuaJson;
+}
+
+const LvHuaJson = {"roads": [
+  { "name":   "高行镇绿地公园养护",
+    "index":  "1",
+    "subroads": [{
+      "area":   "31969.62",
+      "maintain": "东道园"
+    }]
+  },
+
+  { "name":   "东靖路绿化市政设施养护",
+    "index":  "2",
+    "subroads": [{
+      "area":   "9320",
+      "maintain": "东道园"
+    }]
+  },
+
+  { "name":   "申江路公共绿地养护",
+    "index":  "3",
+    "subroads": [{
+      "area":   "12500",
+      "maintain": "东道园"
+    }]
+  },
+
+  { "name":   "赵家沟（申江路-杨高北路）",
+    "index":  "4",
+    "subroads": [{
+      "area":   "8995",
+      "maintain": "萧丰"
+    }]
+  },
+
+  { "name":   "申江路辅道 -东源丽晶西侧绿地",
+    "index":  "5",
+    "subroads": [{
+      "area":   "9500",
+      "maintain": "萧丰"
+    }]
+  },
+
+  { "name":   "东靖路金京路口绿地",
+    "index":  "6",
+    "subroads": [{
+      "area":   "950",
+      "maintain": "萧丰"
+    }]
+  },
+
+  { "name":   "东靖路金高路口绿地",
+    "index":  "7",
+    "subroads": [{
+      "area":   "1290",
+      "maintain": "萧丰"
+    }]
+  },
+
+  { "name":   "高行乐活公园",
+    "index":  "8",
+    "subroads": [{
+      "area":   "42450",
+      "maintain": "东道园"
+    }]
+  },
+
+  { "name":   "高行镇北咸塘浜公共绿地养护",
+    "index":  "1",
+    "subroads": [{
+      "area":   "31486.00",
+      "maintain": "森兰联行"
+    }]
+  },
+
+  { "name":   "高行镇北咸塘浜园路及公共设施维护",
+    "index":  "2",
+    "subroads": [{
+      "area":   "7018.10",
+      "maintain": "森兰联行"
+    }]
+  },
+
+  { "name":   "万安街",
+    "index":  "1",
+    "subroads": [{
+      "area":   "200",
+      "maintain": "佳保"
+    }]
+  },
+
+  { "name":   "双桥路（杨高北路-金高路）",
+    "index":  "2",
+    "subroads": [{
+      "area":   "7800",
+      "maintain": "佳保"
+    }]
+  },
+
+  { "name":   "双桥路（巨峰路-源华路）",
+    "index":  "3",
+    "subroads": [{
+      "area":   "2000",
+      "maintain": "佳保"
+    }]
+  },
+
+  { "name":   "源华路绿地",
+    "index":  "4",
+    "subroads": [{
+      "area":   "9900",
+      "maintain": "佳保"
+    }]
+  },
+
+  { "name":   "俱进路（杨高北路-金高路）",
+    "index":  "5",
+    "subroads": [{
+      "area":   "4500",
+      "maintain": "佳保"
+    }]
+  },
+
+  { "name":   "新行路（幸福居委-东靖路）",
+    "index":  "6",
+    "subroads": [{
+      "area":   "3500",
+      "maintain": "佳保"
+    }]
+  },
+
+  { "name":   "行德路（金京路-新行路）",
+    "index":  "7",
+    "subroads": [{
+      "area":   "800",
+      "maintain": "佳保"
+    }]
+  },
+
+  { "name":   "高行小学围墙外绿地",
+    "index":  "8",
+    "subroads": [{
+      "area":   "150",
+      "maintain": "佳保"
+    }]
+  },
+
+  { "name":   "华高三居花坛养护",
+    "index":  "9",
+    "subroads": [{
+      "area":   "100",
+      "maintain": "佳保"
+    }]
+  },
+
+  { "name":   "行南路（浦东北路-东塘路）",
+    "index":  "10",
+    "subroads": [{
+      "area":   "14000",
+      "maintain": "佳保"
+    }]
+  },
+
+  { "name":   "行泰路（东靖路-金京路）",
+    "index":  "11",
+    "subroads": [{
+      "area":   "1550",
+      "maintain": "佳保"
+    }]
+  },
+
+  { "name":   "衡安路北侧园区绿地",
+    "index":  "1",
+    "subroads": [{
+      "area":   "61750",
+      "maintain": "高南建设"
+    }]
+  },
+
+  { "name":   "高西村黄家头绿地",
+    "index":  "2",
+    "subroads": [{
+      "area":   "18025",
+      "maintain": "高南建设"
+    }]
+  },
+
+  { "name":   "东煦路绿地",
+    "index":  "3",
+    "subroads": [{
+      "area":   "3480",
+      "maintain": "高南建设"
+    }]
+  },
+
+  { "name":   "高行一号绿地",
+    "index":  "1",
+    "subroads": [{
+      "area":   "44700",
+      "maintain": "东外滩园林市政养护公司"
+    }]
+  },
+
+  { "name":   "五洲大道绿地",
+    "index":  "2",
+    "subroads": [{
+      "area":   "69925",
+      "maintain": "东外滩园林市政养护公司"
+    }]
+  }
+]};
